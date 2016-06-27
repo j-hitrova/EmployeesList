70071e689ffef90761dd7d8201f6563eb060a050
Taras Perebeynosov <tperebeynosov@griddynamics.com>
2016-06-28 01:15:53 +0300

    implemented load and save to file

---
+++ src/com/taras/empList/Employee.java
@@ -0,0 +1,10 @@
+package com.taras.empList;
+
+import java.io.Serializable;
+
+/**
+ * Created by tperebeynosov on 28/06/16.
+ */
+public class Employee implements Serializable {
+    String firstname, lastname;
+}
---
+++ src/com/taras/empList/Main.java
@@ -0,0 +1,39 @@
+package com.taras.empList;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String filename = "empList.sav";
+        File file = new File(filename);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        Employee employee = new Employee();
+        ArrayList<Employee> employeeList;
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        employeeList = (ArrayList<Employee>) objectInputStream.readObject();
+        fileInputStream.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        employee.firstname = "Alexander";
+        employee.lastname = "Paul";
+        employeeList.add(employee);
+
+        objectOutputStream.writeObject(employeeList);
+        fileOutputStream.close();
+
+
+        for (Employee employeeOut : employeeList
+                ) {
+            System.out.println(employeeOut.firstname + " " + employeeOut.lastname);
+        }
+    }
+}
