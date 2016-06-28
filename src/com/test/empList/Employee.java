e85f60d4552798a95b248ef6bfae798bf294cad8
Taras Perebeynosov <tperebeynosov@griddynamics.com>
2016-06-28 07:23:09 +0300

    renamed package from taras to test

--- src/com/test/empList/Employee.java
+++ src/com/test/empList/Employee.java
@@ -2,9 +2,6 @@ package com.test.empList;
 
 import java.io.Serializable;
 
-/**
- * Created by tperebeynosov on 28/06/16.
- */
 public class Employee implements Serializable {
     String firstname, lastname;
 }
--- src/com/test/empList/Main.java
+++ src/com/test/empList/Main.java
@@ -6,19 +6,20 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String filename = "empList.sav";
-        File file = new File(filename);
-        if (!file.exists()) {
-            file.createNewFile();
-        }
 
         Employee employee = new Employee();
         ArrayList<Employee> employeeList;
 
-        FileInputStream fileInputStream = new FileInputStream(file);
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-        employeeList = (ArrayList<Employee>) objectInputStream.readObject();
-        fileInputStream.close();
+        String filename = "empList.sav";
+        File file = new File(filename);
+        if (file.exists()) {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            employeeList = (ArrayList<Employee>) objectInputStream.readObject();
+            fileInputStream.close();
+        } else {
+            employeeList = new ArrayList<>();
+        }
 
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
